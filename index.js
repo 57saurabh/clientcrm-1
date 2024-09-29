a1929ec6dccd57c212d15fff1201fa866b496fae
57saurabh <Saurabh.20B0131057@abes.ac.in>
2024-09-29 19:24:27 +0530

    hello

--- index.js
+++ index.js
@@ -7,19 +7,13 @@ const queryRoutes = require('./routes/queryRoutes');
 const app = express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+
 app.use('/api/queries', queryRoutes);
 
-// MongoDB connection
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-// Define a function to handle requests
-app.get('/', (req, res) => {
-  res.send('Welcome to the Express API');
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
-
-// Export the app for Vercel
-module.exports = app;
-
-// Vercel will automatically call the app when deployed
